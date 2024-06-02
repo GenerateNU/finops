4e7144e8bb449a52584671cad17e4fbdb9a765d1
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-06-02 14:55:28 -0400

    Redirect landing page to /requests/reimbursements

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
-import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth();
-  if (!session) return redirect("/auth/login");
+  return redirect("/requests/reimbursements");
 
-  return (
-    <>
-      <div className="p-4 mx-auto w-screen-md bg-slate-100">
-        <pre className="text-xs break-all whitespace-pre-wrap">
-          {JSON.stringify(session, null, 2)}
-        </pre>
-      </div>
+  // const session = await auth();
+  // if (!session) return redirect("/auth/login");
+  //
+  // return (
+  //   <>
+  //     <div className="p-4 mx-auto w-screen-md bg-slate-100">
+  //       <pre className="text-xs break-all whitespace-pre-wrap">
+  //         {JSON.stringify(session, null, 2)}
+  //       </pre>
+  //     </div>
 
-      <div className="mt-12">
-        {/* <FormPoc /> */}
-        {/* <RowPoc /> */}
-        {/* <VoucherPoc /> */}
-      </div>
-    </>
-  );
+  //     <div className="mt-12">
+  //       {/* <FormPoc /> */}
+  //       {/* <RowPoc /> */}
+  //       {/* <VoucherPoc /> */}
+  //     </div>
+  //   </>
+  // );
 }
