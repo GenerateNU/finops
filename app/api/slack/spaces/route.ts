550acbdd3d43944120e1cc93f381cfa91cfd0a9e
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-08-13 13:30:53 -0400

    Format response

--- app/api/slack/spaces/route.ts
+++ app/api/slack/spaces/route.ts
@@ -24,6 +24,18 @@ const slackApp = new SlackApp({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+function formatAccessStatus(status: string): string {
+  switch (status.toLowerCase()) {
+    case "true":
+      return "ACTIVE ✅";
+    case "pending":
+      return "PENDING ⏳";
+    case "false":
+    default:
+      return "UNAUTHORIZED 🚫";
+  }
+}
+
 async function parseFormData(req: Request) {
   const formData = await req.formData();
   const data: { [key: string]: string } = {};
@@ -70,13 +82,62 @@ export async function POST(request: Request) {
       makerspace: member.makerspaceAccess,
     };
 
+    // build response message
+    const response = {
+      blocks: [
+        {
+          type: "header",
+          text: {
+            type: "plain_text",
+            text: "Space Access",
+            emoji: false,
+          },
+        },
+        {
+          type: "context",
+          elements: [
+            {
+              type: "plain_text",
+              text: `Email: ${email}`,
+              emoji: false,
+            },
+          ],
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `The following access has been granted to ${member.fullName}:`,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `*Sherman Center Lobby*: ${formatAccessStatus(spaces.shermLobby)}\n*Studio One*: ${formatAccessStatus(spaces.studioOne)}\n*Makerspace*: ${formatAccessStatus(spaces.makerspace)}`,
+          },
+        },
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Request Access or Report an Issue",
+              },
+              url: process.env.NEXT_PUBLIC_SLACK_HELP_CHANNEL_URL,
+            },
+          ],
+        },
+      ],
+    };
+
     // respond to message
-    return NextResponse.json(
-      {
-        text: `${email} currently has the following access:\n${JSON.stringify(spaces, null, 2)}`,
-      },
-      { status: 200 },
-    );
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
