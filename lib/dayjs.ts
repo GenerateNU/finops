f57cc1dea0e8604bf9d48f7ae2edb2d12bd422cc
Jay Sella <35548468+jaysella@users.noreply.github.com>
2024-08-14 14:03:05 -0400

    Improve timezone handling

--- lib/dayjs.ts
+++ lib/dayjs.ts
@@ -1,7 +1,13 @@
 import dayjs from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 // Extend dayjs with plugins
 dayjs.extend(advancedFormat);
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+dayjs.tz.guess();
 
 export default dayjs;
