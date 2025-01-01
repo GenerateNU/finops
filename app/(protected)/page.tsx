a80df4f4e36cb4e220f56a96a3e9eeae750b9dc6
Jay Sella <35548468+jaysella@users.noreply.github.com>
2025-01-01 12:27:20 -0500

    Add back dashboard unauth redirection

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -1,8 +1,14 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import React from "react";
+
 import { DashboardSkeleton } from "./dashboard-skeleton";
 import Dashboard from "./dashboard.server";
 
 export default async function DashboardPage() {
+  const session = await auth();
+  if (!session) return redirect("/auth/login");
+
   return (
     <>
       <React.Suspense fallback={<DashboardSkeleton />}>
